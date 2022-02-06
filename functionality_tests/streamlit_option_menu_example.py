b1565ed8a4e0b848fdd1a6d79d60de1a6776ec30
AgustinZavalaA <56318577+AgustinZavalaA@users.noreply.github.com>
2022-02-06 11:56:39 -0600

    acomodo de files y prueba de menu

---
+++ functionality_tests/streamlit_option_menu_example.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+with st.sidebar:
+    selected = option_menu(
+        "Main Menu",
+        ["Home", "Settings"],
+        icons=["house", "gear"],
+        menu_icon="cast",
+        default_index=1,
+    )
+    selected
+
+# horizontal menu
+selected2 = option_menu(
+    None,
+    ["Home  ", "Upload", "Tasks", "Settings"],
+    icons=["house", "cloud-upload", "list-task", "gear"],
+    menu_icon="cast",
+    default_index=0,
+    orientation="horizontal",
+)
+selected2
